51cf4a9950ff8d886d40117e2f25847277368256
Ravishankar <ravishankargceb@gmail.com>
2017-03-01 09:51:14 +0000

    testimonial carousel implementation

--- js/main.js
+++ js/main.js
@@ -12,4 +12,17 @@ function MainController($scope,$http) {
         console.log(error);
     });
     
-}
+    $scope.previousTestimonialClick = function(){
+        event.preventDefault();
+        var arrLength = $scope.data.testimonialImg1.length;
+        var splicedImg = $scope.data.testimonialImg1.splice(0,0,$scope.data.testimonialImg1[$scope.data.testimonialImg1.length-1]);
+        $scope.data.testimonialImg1.pop();
+    }
+    
+    $scope.nextTestimonialClick = function(){
+        event.preventDefault();
+        var splicedImg = $scope.data.testimonialImg1.splice(0,1);
+        $scope.data.testimonialImg1.push(splicedImg[0]);
+    }
+    
+};
