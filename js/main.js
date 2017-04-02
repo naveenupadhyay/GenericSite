d53ea0e5a96932777ba0d233edc2fb1bb72598e9
Akshay Jindal <akshayjin@gmail.com>
2017-04-02 12:22:03 +0530

    change popup form ui and added validation

--- js/main.js
+++ js/main.js
@@ -45,6 +45,7 @@ angular.module('mainModule').controller('PopupCont', PopupController);
 function PopupController($scope,$http,$modalInstance) {
     $http.get('data/main.json').then(function(data) {
         $scope.data = data.data;
+        console.log($scope.data);
     },
     function(error){
         console.log(error);
@@ -52,17 +53,41 @@ function PopupController($scope,$http,$modalInstance) {
 
     $scope.showSuccessMessage = "false";
     $scope.formSubmissionMessage = "";
+    $scope.form = {};
     $scope.close = function () {
-        url = "http://localhost:8090/create?email=" + $scope.form.email + "&name=" + $scope.form.name + "&phone=" + $scope.form.phone
-        $http.get(url).then(function(data) {
-            console.log(data);
-            $scope.formSubmissionMessage = data.successMessage;
-        },
-        function(error){
-            console.log(error);
-            $scope.formSubmissionMessage = data.failureMessage;
-        });
+        if(validateForm($scope.form))
+        {
+            $http({
+                method: 'GET',
+                url: encodeURI("http://localhost:8090/create?email=" + $scope.form.email + "&name=" + $scope.form.fname + " " + $scope.form.lname  + "&phone=" + $scope.form.phone),
+                data: {},
+                transformResponse: function (data, headersGetter, status) {
+                    return {data: data};
+                }
+            }).then(function (success){
+                console.log(success);
+                $scope.formSubmissionMessage = $scope.data.successMessage;
+                $scope.form = {};
+           },function (error){
+                console.log(error);
+                $scope.formSubmissionMessage = $scope.data.networkFailureMessage;
+           });
+        } else {
+            $scope.formSubmissionMessage = $scope.data.validationFailureMessage;
+        }
+
         $scope.showSuccessMessage = "true";
         // $modalInstance.dismiss('cancel');
+    };
+
+    var validateForm = function(form) {
+        var emailRegex = /\S+@\S+\.\S+/;
+        if(form.fname == undefined || form.lname == undefined || form.email == undefined || form.phone == undefined)
+            return false;
+        if(!emailRegex.test(form.email))
+            return false;
+        if(isNaN(form.phone) || form.phone.length!=10)
+            return false;
+        return true;
     };
 };
